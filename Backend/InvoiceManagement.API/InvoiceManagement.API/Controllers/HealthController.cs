3567340f395d4e75da0bff1e871d3b600f6e9782
ShrutiSendur <shrutis1211@gmail.com>
2026-02-02 10:43:03 +0530

    feat: backend project structure and initial setup

---
+++ Backend/InvoiceManagement.API/InvoiceManagement.API/Controllers/HealthController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace InvoiceManagement.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("API is running successfully");
+        }
+    }
+}
+    
